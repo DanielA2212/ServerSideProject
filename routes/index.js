668500cf66749da3b91c6c60c9793b827f8134e1
DanielA2212 <DanielAWork98@gmail.com>
2025-06-02 16:30:14 +0300

    added text to the front page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index',
+      { title: 'Welcome To Our Server Side Cost Manager Project' ,
+      body: 'Made By The Greatest Students: Daniel And Nicole'});
 });
 
 module.exports = router;
